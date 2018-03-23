0d2034cbee80e89619bbf2287bae7d783c4996fd
Kieran <32347545+cipher-text@users.noreply.github.com>
2018-03-22 20:42:29 -0400

    dont push creds resource file, first push

---
+++ algonquinCollegeDiscordBot/Modules/Commands.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+using System.IO;
+
+namespace algonquinCollegeDiscordBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("rules"), Summary("Displays the rules of the server")]
+        public async Task Rules()
+        {
+            await Context.Channel.SendMessageAsync(@"
+            Discord Rules:
+            
+            1. -  No Voice/Text/Reaction spam.
+            2. -  No Racism/Gore/NSFW.
+            3. -  Do not share personal information.
+            4. -  No extreme harassment/flaming of users within the discord.
+            5. -  Do not chat excessively in other languages.
+            6. -  No advertising. (Unless you are a regular of the discord)
+            
+            All moderators can punish at their own discretion. A warning will be issued if continuous it will lead to a mute. In extreme cases, you may be banned. 
+            These rules are tentative and punishments are resolved on a case-by-case basis.
+            ");
+        }
+        [Command("commands"), Summary("Displays the current commands available to the discord user by enumerating the Modules directory.")]
+        public async Task commands()
+        {
+            await Context.Channel.SendMessageAsync(
+                "Current Supported Commands: \n" +
+                "\n" +
+                "!commands\n" +
+                "!invite\n" +
+                "!rules\n" +
+                "!sourcecode");
+
+        }
+        [Command("invite"), Summary("Displays the permanent invite link for the Algonquin College discord.")]
+        public async Task Invite()
+        {
+            await Context.Channel.SendMessageAsync("https://discord.gg/VwukBWK");
+        }
+
+        [Command("sourcecode"), Summary("Displays the github repo for this discord bot.")]
+        public async Task Sourcecode()
+        {
+            await Context.Channel.SendMessageAsync("github.com/kieran-github");
+        }
+    }
+}
---
+++ algonquinCollegeDiscordBot/entryPoint.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+
+namespace algonquinCollegeDiscordBot
+{
+    public class EntryPoint
+    {
+        private DiscordSocketClient _client;
+        private CommandHandler _handler;
+
+        public static void Main(string[] args) => new EntryPoint().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _handler = new CommandHandler(_client);
+
+            await _client.LoginAsync(TokenType.Bot, creds.token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
